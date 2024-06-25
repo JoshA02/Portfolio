414f644a60951c60225fe589b8cf1d9b3cc46c3f
Josh Villyat <joshaaron02@outlook.com>
2024-06-26 00:20:44 +0100

    Implemented server-side form handling

---
+++ actions/submitContact.tsx
@@ -0,0 +1,15 @@
+'use server';
+
+export async function submitContact(_prevState: any, formData: FormData) {
+  console.log('submitContact', formData.get('email'), formData.get('message'));
+  
+  // Validate form
+  if (!formData.get('email') || !formData.get('message')) {
+    return {message: 'Please fill out all fields'};
+  }
+
+  // wait 4 seconds
+  await new Promise((resolve) => setTimeout(resolve, 4000));
+
+  return {message: 'Form submitted!'};
+}
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,22 @@
 'use client';
 
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import './style.css';
+import {useFormState, useFormStatus} from 'react-dom';
+import {submitContact} from '@/actions/submitContact';
+
+const initialFormState = {
+  email: '',
+  message: ''
+};
+
+function SubmitButton() {
+  const {pending} = useFormStatus();
+  return <button disabled={pending} className='bg-container'>Send</button>
+}
 
 export default function Contact() {
+  const [formState, formAction] = useFormState(submitContact, initialFormState);
   const [charCount, setCharCount] = useState(0);
   const [doShake, setDoShake] = useState(false);
   const maxLength = 2500;
@@ -23,30 +36,29 @@ export default function Contact() {
 
   // Submit form
   function submitForm(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-
-    if(e.currentTarget.checkValidity()){
-      return alert('Form submitted!');
+    if(!e.currentTarget.checkValidity()){
+      alert('Please fill out all fields correctly');
+      return e.preventDefault();
     }
-
-    alert('Please fill in the form correctly.');
+    // Allow form submission
   }
 
   return (
-    <form onSubmit={(e) => submitForm(e)} noValidate className='bg-container px-4 py-7 rounded-t-md md:w-3/4 lg:w-1/2 xl:w-1/3 w-3/4 flex flex-col'>
+    <form action={formAction} onSubmit={(e) => submitForm(e)} noValidate className='bg-container px-4 py-7 rounded-t-md md:w-3/4 lg:w-1/2 xl:w-1/3 w-3/4 flex flex-col'>
       <h1>Get in touch!</h1>
-      
+      <span>{formState.message}</span>
+
       <span>email</span>
-      <input type='email' required placeholder='email@domain.com'/>
+      <input type='email' name='email' required placeholder='email@domain.com'/>
 
       <span>message</span>
       <div className={'flex flex-col relative mb-3' + (doShake ? ' shake' : '')}>
-        <textarea required placeholder='Your message here' className='min-h-80 mb-0 resize-y max-h-full' maxLength={maxLength} onKeyDown={(e) => updateCharCount(e)}/>
+        <textarea required name='message' placeholder='Your message here' className='min-h-80 mb-0 resize-y max-h-full' maxLength={maxLength} onKeyDown={(e) => updateCharCount(e)}/>
 
         {/* Char count: */}
         <span className={'text-right absolute right-2 bottom-0' + (charCount >= maxLength ? ' urgent animate' : '')}>{charCount}/{maxLength}</span>
       </div>
-      <button className='bg-container'>Send</button>
+      <SubmitButton/>
     </form>
   );
 }
