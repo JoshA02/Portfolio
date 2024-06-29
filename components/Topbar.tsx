5dbc399f7e858f1c6f9b461adbfb1cb1bd8edfb5
Josh Villyat <joshaaron02@outlook.com>
2024-06-29 15:43:39 +0100

    Added filler div to fix topbar centering (after removing personalisation section)

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -27,9 +27,7 @@ export default function Topbar() {
           <TopbarLink key={item.href} item={item} />
         ))}
       </div>
-      {/* <div className='personalise'>
-        ...
-      </div> */}
+      <div/>
     </nav>
   )
 };
