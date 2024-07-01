4383abc3a98a14c43419f96c9ff35d141ce23654
Josh Villyat <joshaaron02@outlook.com>
2024-07-01 23:48:25 +0100

    Adjusted global css to affect all scrollbars, not just ones inside text areas. Also added slight padding to featuredProjects to keep the bar off the projects themselves

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -8,7 +8,7 @@ export default function Projects() {
       <div className='mb-16'>
         <SectionHeader title='featuredProjects' subtitle='made with care'/>
 
-        <div className='w-full overflow-x-scroll'>
+        <div className='w-full overflow-x-scroll pb-2'>
           <div className='flex w-max gap-6'>
             <Project title='Evently' link='https://github.com/JoshA02/Evently' coverImage='/project-images/evently-admin.png' miniImages={['/typescript.svg', '/aspnet.svg', '/cs.svg', '/cshtml.svg']} description='An events-browsing platform built using ASP.NET Core Razor Pages, allowing users to host, discover and register for upcoming events. User roles are also implemented, giving admins additional control over other users (password resets, credential changes, etc).'/>
             <Project title='Portfolio' link='https://github.com/JoshA02/CinemaShowtimes' coverImage='/project-images/portfolio-home.png' miniImages={['/nextjs.svg', '/react.svg', '/tailwind.svg', '/typescript.svg', '/expressjs.svg', '/docker.svg']} description="Quisque non semper sem, eget venenatis nunc. Pellentesque aliquet ligula id feugiat accumsan. Integer congue placerat dapibus. Duis sodales iaculis urna, a varius turpis fringilla vel. Nullam tincidunt magna ac interdum bibendum. Fusce viverra ligula nec diam luctus euismod."/>
