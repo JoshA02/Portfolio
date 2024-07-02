73c5a84197229009725035d0f460d748a450e9f9
Josh Villyat <joshaaron02@outlook.com>
2024-07-02 13:56:00 +0100

    Further adjusted styling and adjusted CSS breakpoint on home page

--- components/Project.tsx
+++ components/Project.tsx
@@ -14,13 +14,13 @@ interface ProjectProps {
 
 export default function Project({title, miniImages, description, coverImage, link, linkType='github', fullWidth}: ProjectProps) {
   return (
-    <div className={'bg-container rounded-lg p-2 relative overflow-hidden animate-fade ' + (fullWidth ? 'w-full h-52 bg-opacity-30' : 'max-w-80 aspect-video')}>
+    <div className={'bg-container rounded-lg p-2 relative overflow-hidden animate-fade group ' + (fullWidth ? 'w-full h-52 bg-opacity-30' : 'max-w-80 aspect-video')}>
       
       {/* Project Cover Image */}
       {coverImage && 
-      <div className={'absolute w-full h-full left-0 top-0 transition-opacity group '}>
-        <div className={'absolute opacity-20 transition-all ' + (fullWidth ? 'animate-float hidden md:inline group-hover:translate-y-2 right-20 group-hover:opacity-40' : 'group-hover:opacity-25 group-hover:rotate-1 scale-150')}>
-          <Image src={coverImage} alt={coverImage} width={1000} height={1000} className={'relative transition-transform ' + (fullWidth ? 'scale-250 h-52 w-auto rotate-6' : 'w-full')}/>
+      <div className={'absolute w-full h-full left-0 top-0 transition-opacity -z-10'}>
+        <div className={'absolute opacity-20 transition-all ' + (fullWidth ? 'animate-float hidden md:inline right-20 group-hover:opacity-80' : 'group-hover:opacity-35 group-hover:rotate-1 scale-150')}>
+          <Image src={coverImage} alt={coverImage} width={1000} height={1000} className={'relative transition-transform ' + (fullWidth ? 'scale-250 h-52 w-auto rotate-6 group-hover:-translate-x-3' : 'w-full')}/>
         </div>
       </div>}
 
@@ -43,7 +43,7 @@ export default function Project({title, miniImages, description, coverImage, lin
       </p>
 
       {/* Project Link */}
-      {link && <Link target='_blank' href={link} className='button absolute bottom-2 right-2 bg-container bg-opacity-0 text-white rounded-md p-1.5 hover:bg-opacity-50 transition-colors flex items-center justify-center min-w-24'>
+      {link && <Link target='_blank' href={link} className='button absolute bottom-2 right-2 bg-container bg-opacity-0 text-white rounded-md p-1.5 group-hover:bg-opacity-50 hover:bg-black transition-colors flex items-center justify-center min-w-24'>
         {linkType == 'github' && <Image src='/github.png' alt={linkType} width={512} height={512} className='w-4 h-4 mr-1'/>}
         <span>{linkType == 'github' ? 'Clone' : 'Visit'}</span>
       </Link>}
