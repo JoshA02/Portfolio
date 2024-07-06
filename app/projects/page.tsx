200ebbc2501c42c023dab6624cd29b3b61b614ce
Josh <joshuav2012@live.co.uk>
2024-07-06 16:44:59 +0100

    Finished copy for projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -8,9 +8,18 @@ export default function Projects() {
       <div className="mb-16">
         <SectionHeader title='featured' subtitle='my favourites'/>
         <ProjectContainer type='carrossel'>
-          <Project title='Evently' link='https://github.com/JoshA02/Evently' coverImage='evently-admin' miniImages={['/project-tools/typescript.png', '/project-tools/aspnet.png', '/project-tools/cs.png', '/project-tools/cshtml.png']} description='An events-browsing platform built using ASP.NET Core Razor Pages, allowing users to host, discover and register for upcoming events. User roles are also implemented, giving admins additional control over other users (password resets, credential changes, etc).'/>
-          <Project title='Portfolio' link='https://github.com/JoshA02/Portfolio' coverImage='portfolio-home' miniImages={['/project-tools/nextjs.png', '/project-tools/react.png', '/project-tools/tailwind.png', '/project-tools/typescript.png', '/project-tools/expressjs.png', '/project-tools/docker.png']} description="Quisque non semper sem, eget venenatis nunc. Pellentesque aliquet ligula id feugiat accumsan. Integer congue placerat dapibus. Duis sodales iaculis urna, a varius turpis fringilla vel. Nullam tincidunt magna ac interdum bibendum. Fusce viverra ligula nec diam luctus euismod."/>
-          <Project title='CinemaShowtimes' link='https://github.com/JoshA02/CinemaShowtimes' coverImage='cinema-schedule' miniImages={['/project-tools/typescript.png', '/project-tools/expressjs.png', '/project-tools/react.png', '/project-tools/docker.png']} description="Developed for educational purposes to demonstrate skills in React, Docker, ExpressJS, and API integration. It provides an example of how to access and display showtimes and movie titles from a cinema's API."/>
+          <Project
+            title='Evently' link='https://github.com/JoshA02/Evently' coverImage='evently-admin' miniImages={['/project-tools/typescript.png', '/project-tools/aspnet.png', '/project-tools/cs.png', '/project-tools/cshtml.png']}
+            description='An events-browsing platform built using ASP.NET Core Razor Pages, allowing users to host, discover and register for upcoming events. User roles are also implemented, giving admins additional control over other users (password resets, credential changes, etc).'
+          />
+          <Project
+            title='Portfolio' link='https://github.com/JoshA02/Portfolio' coverImage='portfolio-home' miniImages={['/project-tools/nextjs.png', '/project-tools/react.png', '/project-tools/tailwind.png', '/project-tools/typescript.png', '/project-tools/docker.png']}
+            description="Built using Next.js, React, Tailwind CSS, and TypeScript. The site is fully responsive and includes a 'contact me' form, protected by Google's reCAPTCHA v2, with messages forwarded to myself with Postmark. Hosted on my own server, running in a Docker container and reverse-proxied via Cloudflare Tunnels."
+          />
+          <Project
+            title='CinemaShowtimes' link='https://github.com/JoshA02/CinemaShowtimes' coverImage='cinema-schedule' miniImages={['/project-tools/typescript.png', '/project-tools/expressjs.png', '/project-tools/react.png', '/project-tools/docker.png']}
+            description="Developed for educational purposes to demonstrate skills in React, Docker, ExpressJS, and API integration. It provides an example of how to access and display showtimes and movie titles from a cinema's API."
+          />
         </ProjectContainer>
       </div>
 
@@ -18,7 +27,10 @@ export default function Projects() {
         <SectionHeader title='web' subtitle='full-stack'/>
         <ProjectContainer>
           <Project fullWidth title='Evently' link='https://github.com/JoshA02/Evently' coverImage='evently-admin' miniImages={['/project-tools/typescript.png', '/project-tools/aspnet.png', '/project-tools/cs.png', '/project-tools/cshtml.png']} description='An events-browsing platform built using ASP.NET Core Razor Pages, allowing users to host, discover and register for upcoming events. User roles are also implemented, giving admins additional control over other users (password resets, credential changes, etc).'/>
-          <Project fullWidth title='Portfolio' link='https://github.com/JoshA02/Portfolio' coverImage='portfolio-home' miniImages={['/project-tools/nextjs.png', '/project-tools/react.png', '/project-tools/tailwind.png', '/project-tools/typescript.png', '/project-tools/expressjs.png', '/project-tools/docker.png']} description="Quisque non semper sem, eget venenatis nunc. Pellentesque aliquet ligula id feugiat accumsan. Integer congue placerat dapibus. Duis sodales iaculis urna, a varius turpis fringilla vel. Nullam tincidunt magna ac interdum bibendum. Fusce viverra ligula nec diam luctus euismod."/>
+          <Project
+            fullWidth title='Portfolio' link='https://github.com/JoshA02/Portfolio' coverImage='portfolio-home' miniImages={['/project-tools/nextjs.png', '/project-tools/react.png', '/project-tools/tailwind.png', '/project-tools/typescript.png', '/project-tools/docker.png']}
+            description="Built using Next.js, React, Tailwind CSS, and TypeScript. The site is fully responsive and includes a 'contact me' form, protected by Google's reCAPTCHA v2, with messages forwarded to myself with Postmark. Hosted on my own server, running in a Docker container and reverse-proxied via Cloudflare Tunnels."
+          />
           <Project fullWidth title='CinemaShowtimes' link='https://github.com/JoshA02/CinemaShowtimes' coverImage='cinema-schedule' miniImages={['/project-tools/typescript.png', '/project-tools/expressjs.png', '/project-tools/react.png', '/project-tools/docker.png']} description="Developed for educational purposes to demonstrate skills in React, Docker, ExpressJS, and API integration. It provides an example of how to access and display showtimes and movie titles from a cinema's API."/>
         </ProjectContainer>
       </div>
@@ -26,8 +38,14 @@ export default function Projects() {
       <div className="mb-16">
         <SectionHeader title='spigot' subtitle='minecraft server plugins'/>
         <ProjectContainer>
-          <Project fullWidth title='Manhunt Plus' linkType='other' link='https://www.spigotmc.org/resources/manhunt-plus-free-1-8-1-17-1.81888/' miniImages={['/project-tools/typescript.png', '/project-tools/aspnet.png', '/project-tools/cs.png', '/project-tools/cshtml.png']} description='Quisque non semper sem, eget venenatis nunc. Pellentesque aliquet ligula id feugiat accumsan. Integer congue placerat dapibus. Duis sodales iaculis urna, a varius turpis fringilla vel. Nullam tincidunt magna ac interdum bibendum. Fusce viverra ligula nec diam luctus euismod.'/>
-          <Project fullWidth title='MoneyDrops' linkType='other' link='https://www.spigotmc.org/resources/money-drops-1-8-1-18.78789/' miniImages={['/project-tools/typescript.png', '/project-tools/aspnet.png', '/project-tools/cs.png', '/project-tools/cshtml.png']} description='Quisque non semper sem, eget venenatis nunc. Pellentesque aliquet ligula id feugiat accumsan. Integer congue placerat dapibus. Duis sodales iaculis urna, a varius turpis fringilla vel. Nullam tincidunt magna ac interdum bibendum. Fusce viverra ligula nec diam luctus euismod.'/>
+          <Project
+            fullWidth title='Manhunt Plus' linkType='other' link='https://www.spigotmc.org/resources/manhunt-plus-free-1-8-1-17-1.81888/' miniImages={['/project-tools/java.png', '/project-tools/spigot.png']}
+            description='A Minecraft Server plugin that adds the popular "Manhunt" gamemode to your server; written in Java. The plugin is highly configurable and includes a variety of features to enhance the gameplay experience, including timed air drops, compass tracking, and more.'
+          />
+          <Project
+            fullWidth title='MoneyDrops' linkType='other' link='https://www.spigotmc.org/resources/money-drops-1-8-1-18.78789/' miniImages={['/project-tools/java.png', '/project-tools/spigot.png']}
+            description='A Minecraft Server plugin that allows players to receive money drops from mobs they kill. The plugin is highly configurable and includes a variety of features to enhance the gameplay experience, including custom mob drops, drop rates, and more.'
+          />
         </ProjectContainer>
       </div>
 
