f7a35986c8e18a82a9eca29b58d2c45e364bd5e2
Josh Villyat <joshaaron02@outlook.com>
2024-12-20 05:58:19 +0000

    visually removed loading spinner

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Loading() {
   return (
     <main className='flex justify-center mb-12'>
-      <Image src='/spinner.gif' alt='loading' width={100} height={100} />
+      {/* <Image src='/spinner.gif' alt='loading' width={100} height={100} /> */}
     </main>
   );
 }
