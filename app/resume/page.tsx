2820a16b470e68095f50a9a0c2cc47b2342a1592
Josh Villyat <joshaaron02@outlook.com>
2025-02-23 20:40:55 -0700

    resume changed

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Resume() {
   return (
     <main className='flex justify-center mb-12'>
-      <Image src='/resume.jpg' alt='resume' width={800} height={1131} />
+      <Image src='/resume.jpg' alt='resume' width={2480} height={3508} className='w-auto h-screen' />
     </main>
   )
 }
