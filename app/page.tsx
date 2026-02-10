82407ef05d86e61bf8aac5338336443000d07551
Josh Villyat <hello@joshaaron.me>
2026-02-10 15:23:27 +0000

    Create Card component for reusability across home

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
+import Card, { CardButton } from '@/components/Card';
+
 export default function Home() {
   return (
     <div className="flex min-h-screen justify-center font-body">
       <main>
-        <h1 className="text-4xl text-foreground">Hello, world!</h1>
+        <div className='gap-4 grid grid-cols-2'>
+          <Card id="home" backgroundImage="/icon/uk.png" tagline="WELCOME">
+            <h3 className='text-lg text-foreground font-title font-semibold'>Josh Aaron Villyat</h3>
+            <p className='font-body text-card-fg'>A junior full-stack dev from the UK studying Software Engineering @ NTU</p>
+            <div className='flex gap-4 mt-4'>
+              <CardButton href="http://github.com/JoshA02" icon="github.svg" alt="github">GITHUB</CardButton>
+              <CardButton href="mailto:hello@joshaaron.me" icon="email.svg" alt="email">EMAIL</CardButton>
+              <CardButton href="http://linkedin.com/in/josh-villyat" icon="linkedin.svg" alt="linkedin">LINKEDIN</CardButton>
+            </div>
+          </Card>
+        </div>
       </main>
     </div>
   );
---
+++ components/Card.tsx
@@ -0,0 +1,59 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface CardProps {
+  id?: string;
+  children: React.ReactNode;
+  backgroundImage?: string;
+  tagline?: string;
+}
+
+export default function Card({ id, children, backgroundImage, tagline }: CardProps) {
+  return (
+    <section 
+      id={id}
+      className='gradient-card-border bg-card-bg rounded-xl p-4 w-max relative overflow-hidden z-10'
+      style={{
+        boxShadow: "inset -179px 0px 250px -45px rgba(24, 55, 89, 10%)"
+      }}
+    >
+      {backgroundImage && (
+        <Image 
+          src={backgroundImage} 
+          alt='' 
+          width={0} 
+          height={0} 
+          sizes='100vw' 
+          className='w-full h-auto absolute top-0 -right-40 -z-10 -translate-y-1/4 scale-50'
+        />
+      )}
+      {tagline && (
+        <h2 className='text-sm font-heading text-accent tracking-widest font-medium'>{"// " + tagline}</h2>
+      )}
+      {children}
+    </section>
+  );
+}
+
+interface CardButtonProps {
+  href: string;
+  icon: string;
+  alt: string;
+  children: React.ReactNode;
+}
+
+export function CardButton({ href, icon, alt, children }: CardButtonProps) {  
+  return (
+    <Link href={href} target="_blank"
+      className='text-sm text-accent border border-transparent rounded-xl
+                px-4 py-2 font-bold active:scale-95 font-body
+                flex items-center transition-transform duration-100'
+      style={{
+        background: "linear-gradient(to bottom, var(--accent-faded), var(--accent-faded)) padding-box, linear-gradient(to bottom, hsl(from var(--accent) h s calc(l + 10)), hsl(from var(--accent) h s calc(l - 10))) border-box"
+      }}
+    >
+      <Image src={"icon/" + icon} alt={alt} width={16} height={16} className='inline-block mr-2'/>
+      {children}
+    </Link>
+  );
+}
